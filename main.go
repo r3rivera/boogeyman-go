e5015588d63179ce8541bf16965a49da4e0b1121
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-05-27 18:17:17 -0500

    Added interface to handle rendering of the qrcode

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ import (
 func main() {
 
 	p := b_qrcode.Person{
-		FirstName: "Rommel Ryan",
-		LastName:  "Rivera",
-		Street:    "1420 W MCDERMOTT DR., #417",
-		City:      "Allen",
-		Zip:       "75013",
-		State:     "TEXAS",
-		Country:   "USA",
-		Phone:     "+1 (650) 773-1121",
+		FirstName: "",
+		LastName:  "",
+		Street:    "",
+		City:      "",
+		Zip:       "",
+		State:     "",
+		Country:   "",
+		Phone:     "",
 	}
 	fmt.Println(p.Stringify())
 
 	c := b_qrcode.ContactInfo{
-		Email: "rr.rivs@gmail.com",
+		Email: "",
 	}
 
 	fmt.Println(c.Stringify())
