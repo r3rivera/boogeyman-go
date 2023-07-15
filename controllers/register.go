b06bf1e0bfea4576380e5f16764d3e6a27b8bd7a
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-07-15 09:40:32 -0500

    Added basic registration

--- controllers/register.go
+++ controllers/register.go
@@ -1,7 +1,24 @@
 package controllers
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type UserReg struct {
-	firstName string
-	lastName  string
-	email     string
+	FirstName string `json:"firstname" binding:"required"`
+	LastName  string `json:"lastname" binding:"required"`
+	Email     string `json:"email" binding:"required"`
+}
+
+func RegisterUser(c *gin.Context) {
+	var registration UserReg
+	if err := c.ShouldBindJSON(&registration); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "OK"})
+
 }
