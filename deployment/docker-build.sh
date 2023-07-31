7b178c8b8b9c023e7fb26a7e43ea3138959d7ec0
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-07-30 19:00:54 -0500

    Fix docker build order

--- deployment/docker-build.sh
+++ deployment/docker-build.sh
@@ -11,6 +11,17 @@ echo "Start Creating Docker Image"
 chmod +x ./deployment/Dockerfile
 DOCKER_TAG="boogeyman-go"
 
+DOCKER_CTR_STOP="$(docker container stop ${DOCKER_TAG} | cut -d ':' -f 2)"
+echo "Stop Status :: ${DOCKER_CTR_STOP}"
+
+echo "Removing stopped container..."
+DOCKER_CTR_RM="$(docker container rm ${DOCKER_TAG})"
+echo "Remove Status :: ${DOCKER_CTR_RM}"
+
+DOCKER_CURR="$(docker image rm ${DOCKER_TAG})"
+echo "Removing existing docker images..."
+echo "Remove Status :: ${DOCKER_CURR}"
+
 GIT_COMMIT_LABEL="$(git log | head -n 1 | cut -d '/' -f 2)"
 
 docker build -t ${DOCKER_TAG} --no-cache -f ./deployment/Dockerfile .
--- deployment/docker-deploy.sh
+++ deployment/docker-deploy.sh
@@ -11,18 +11,6 @@ echo "Stopping an existing docker container..."
 
 DOCKER_TAG="boogeyman-go"
 
-
-DOCKER_CTR_STOP="$(docker container stop ${DOCKER_TAG} | cut -d ':' -f 2)"
-echo "Stop Status :: ${DOCKER_CTR_STOP}"
-
-echo "Removing stopped container..."
-DOCKER_CTR_RM="$(docker container rm ${DOCKER_TAG})"
-echo "Remove Status :: ${DOCKER_CTR_RM}"
-
-DOCKER_CURR="$(docker image rm ${DOCKER_TAG})"
-echo "Removing existing docker images..."
-echo "Remove Status :: ${DOCKER_CURR}"
-
 echo "Start deploying docker image"
 echo ""
 echo "Deploying a new docker container...."
