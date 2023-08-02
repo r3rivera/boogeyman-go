9885ed846e331c3702607bbc5e664d1fdcdf5a2b
RR Rivera <r2devops@R2MacPRO.attlocal.net>
2023-08-01 20:45:06 -0500

    Fix to environment break 1

--- deployment/docker-deploy.sh
+++ deployment/docker-deploy.sh
@@ -14,13 +14,13 @@ DOCKER_TAG="boogeyman-go"
 echo "Start deploying docker image"
 echo ""
 echo "Deploying a new docker container...."
-
+echo "AWS $(aws --profile default configure get aws_access_key_id)"
 AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
 AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
 DOCKER_UUID="$(docker container run  \
     -p 8082:8082 \
-    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+    -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
     -d --name ${DOCKER_TAG} ${DOCKER_TAG})"
 echo "New Docker ID deployed :: ${DOCKER_UUID}"
